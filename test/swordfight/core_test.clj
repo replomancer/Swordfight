246a3fe0db266d5e71de952617694df71fe876d6
Lukasz Kozuchowski <Lukasz.Kozuchowski@gmail.com>
2015-02-09 00:09:20 +0100

    Test bug fix

--- test/swordfight/core_test.clj
+++ test/swordfight/core_test.clj
@@ -2,10 +2,7 @@
   (:use clojure.test
         [swordfight.core :only [initial-settings]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
 
 (deftest settings-test
   (testing "Oooh, a test"
-    (is ((:xboard-mode initial-settings) false))))
+    (is (false? (:xboard-mode initial-settings)))))
