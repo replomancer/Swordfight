2e10040c0ac675923d2ae88d49cb01b5d988caf5
Lukasz Kozuchowski <Lukasz.Kozuchowski@gmail.com>
2015-03-25 21:43:24 +0100

    Moving pieces on board

--- src/swordfight/cecp.clj
+++ src/swordfight/cecp.clj
@@ -1,4 +1,5 @@
-(ns swordfight.cecp)
+(ns swordfight.cecp
+  (:use [swordfight.game-rules :only [move]]))
 
 
 (defn xboard [game-state game-settings _]
@@ -12,10 +13,18 @@
   [(assoc game-state :quitting true) game-settings "Finished."])
 
 
+(defn MOVE [game-state game-settings cmd-vector]
+  (let [algebraic-notation (first cmd-vector)
+        pos1 (subs algebraic-notation 0 2) ;; FIXME: notation parsing
+        pos2 (subs algebraic-notation 2 4)]
+    [(assoc game-state :board
+            (move (:board game-state) pos1 pos2))
+     game-settings
+     ""]))
+
 (defn eval-command [game-state game-settings cmd-vector]
   (let [cmd (get cmd-vector 0)
         cmd-fun (cond (= cmd "quit") quit
                       (= cmd "xboard") xboard
-                      :else
-                      (fn [_ _ _] [game-state game-settings ""]))]
+                      :else MOVE)]
     (cmd-fun game-state game-settings cmd-vector)))
