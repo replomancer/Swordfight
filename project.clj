a530fe7f8f6813e62a3423244a7985f71e0c55d1
Lukasz Kozuchowski <Lukasz.Kozuchowski@gmail.com>
2015-03-31 23:46:05 +0200

    Clojure version 1.7.0-alpha6

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :main swordfight.core
   :license {:name "2-clause BSD"
             :url ""}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]])
+  :dependencies [[org.clojure/clojure "1.7.0-alpha6"]])
