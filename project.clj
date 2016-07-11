640ae00ac4dde1983eab46d200e19cbf7a3f0121
Lukasz Kozuchowski <Lukasz.Kozuchowski@gmail.com>
2016-07-11 15:53:35 +0200

    Clojure version 1.9.0-alpha9

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :main swordfight.core
   :license {:name "2-clause BSD"
             :url ""}
-  :dependencies [[org.clojure/clojure "1.8.0-alpha2"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha9"]])
