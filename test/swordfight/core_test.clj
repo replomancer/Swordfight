ecbc120c6e0b7d176b045bdaa0cf6cc290cf927a
Lukasz Kozuchowski <Lukasz.Kozuchowski@gmail.com>
2016-07-13 23:48:42 +0200

    Tests rewritten to use Midje

--- test/swordfight/core_test.clj
+++ test/swordfight/core_test.clj
@@ -1,5 +1,5 @@
 (ns swordfight.core-test
-  (:use clojure.test
+  (:use [midje.sweet :only [facts fact]]
         [swordfight.core :only [initial-settings]]
         [swordfight.game-rules :only [initial-board change-side move
                                       board-coords board-notation empty-square?]]
@@ -22,90 +22,103 @@
                                        [square-from square-to]))))))
 
 
-(deftest moves-generation-test
-  (testing "How many moves are generated. Compare against known values."
-    (is (= (legal-moves-cnt-in-turn 1) 20))
-    (is (= (legal-moves-cnt-in-turn 2) 400))
-    (is (= (legal-moves-cnt-in-turn 3) 8902))
-    (is (= (legal-moves-cnt-in-turn 4) 197742))
-    ;; TODO:
-    ;; - The test fails for turn 5
-    ;; - Find the right value for turn 6
-    ;; - Improve the test speed
-    ;;(is (= (legal-moves-cnt-in-turn 5) 4897256))
-    ;;(is (= (legal-moves-cnt-in-turn 6) 120000000))
-    ))
+(facts "about numbers of moves"
+  ;; "How many moves are generated. Compare against known values."
+  (fact (legal-moves-cnt-in-turn 1) => 20)
+  (fact (legal-moves-cnt-in-turn 2) => 400)
+  (fact (legal-moves-cnt-in-turn 3) => 8902)
+  (fact (legal-moves-cnt-in-turn 4) => 197742)
+  ;; TODO:
+  ;; - The test fails for turn 5
+  ;; - Find the right value for turn 6
+  ;; - Improve the test speed
+  ;;(fact (legal-moves-cnt-in-turn 5) => 4897256)
+  ;;(fact (legal-moves-cnt-in-turn 6) => 120000000)
+)
 
 
-(deftest en-passant-test-case1
-  (testing "Engine knows en passant moves 1."
-    (let [board
-          [[ "BR"  "BN"  "BB"  "BQ"  "BK"  "BB"  "BN"  "BR" ]
-           [ "BP"  "  "  "BP"  "BP"  "BP"  "BP"  "BP"  "BP" ]
-           [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
-           [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
-           [ "WP"  "BP"  "  "  "  "  "  "  "  "  "  "  "  " ]
-           [ "  "  "  "  "  "  "WP"  "  "  "WN"  "  "  "  " ]
-           [ "  "  "WP"  "WP"  "  "  "WP"  "WP"  "WP"  "WP" ]
-           [ "WR"  "WN"  "WB"  "WQ"  "WK"  "WB"  "  "  "WR" ]]
-          last-move ["a2" "a4"]
-          found-moves (set (map (partial map board-notation)
-                                     (find-available-moves \B board last-move)))]
-      (is (contains? found-moves ["b4" "a3"]))
-      (is (empty-square? (get-in (move board "b4" "a3") (board-coords "a4")))))))
+(facts "about en passant moves"
+  (let [board
+        [[ "BR"  "BN"  "BB"  "BQ"  "BK"  "BB"  "BN"  "BR" ]
+         [ "BP"  "  "  "BP"  "BP"  "BP"  "BP"  "BP"  "BP" ]
+         [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "WP"  "BP"  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "  "  "  "  "  "  "WP"  "  "  "WN"  "  "  "  " ]
+         [ "  "  "WP"  "WP"  "  "  "WP"  "WP"  "WP"  "WP" ]
+         [ "WR"  "WN"  "WB"  "WQ"  "WK"  "WB"  "  "  "WR" ]]
+        board'
+        [[ "BR"  "BN"  "BB"  "BQ"  "BK"  "BB"  "BN"  "BR" ]
+         [ "BP"  "  "  "BP"  "BP"  "BP"  "BP"  "BP"  "BP" ]
+         [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "BP"  "  "  "  "  "WP"  "  "  "WN"  "  "  "  " ]
+         [ "  "  "WP"  "WP"  "  "  "WP"  "WP"  "WP"  "WP" ]
+         [ "WR"  "WN"  "WB"  "WQ"  "WK"  "WB"  "  "  "WR" ]]
+        last-move ["a2" "a4"]
+        found-moves (set (map (partial map board-notation)
+                              (find-available-moves \B board last-move)))]
+    (fact "Engine knows en passant moves to the left"
+      (contains? found-moves ["b4" "a3"]) => true)
+    (fact "Engine understands results of en passant moves to the left"
+      (move board "b4" "a3") => board'))
 
 
-(deftest en-passant-test-case2
-  (testing "Engine knows en passant moves 2."
-    (let [board
-          [[ "BR"  "BN"  "BB"  "BQ"  "BK"  "BB"  "BN"  "BR" ]
-           [ "BP"  "  "  "BP"  "BP"  "BP"  "BP"  "BP"  "BP" ]
-           [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
-           [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
-           [ "  "  "BP"  "WP"  "  "  "  "  "  "  "  "  "  " ]
-           [ "  "  "  "  "  "  "WP"  "  "  "WN"  "  "  "  " ]
-           [ "WP"  "WP"  "  "  "  "  "WP"  "WP"  "WP"  "WP" ]
-           [ "WR"  "WN"  "WB"  "WQ"  "WK"  "WB"  "  "  "WR" ]]
-          last-move ["c2" "c4"]
-          found-moves (set (map (partial map board-notation)
-                                     (find-available-moves \B board last-move)))]
-      (is (contains? found-moves ["b4" "c3"]))
-      (is (empty-square? (get-in (move board "b4" "c3") (board-coords "c4")))))))
+  (let [board
+        [[ "BR"  "BN"  "BB"  "BQ"  "BK"  "BB"  "BN"  "BR" ]
+         [ "BP"  "  "  "BP"  "BP"  "BP"  "BP"  "BP"  "BP" ]
+         [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "  "  "BP"  "WP"  "  "  "  "  "  "  "  "  "  " ]
+         [ "  "  "  "  "  "  "WP"  "  "  "WN"  "  "  "  " ]
+         [ "WP"  "WP"  "  "  "  "  "WP"  "WP"  "WP"  "WP" ]
+         [ "WR"  "WN"  "WB"  "WQ"  "WK"  "WB"  "  "  "WR" ]]
+        board'
+        [[ "BR"  "BN"  "BB"  "BQ"  "BK"  "BB"  "BN"  "BR" ]
+         [ "BP"  "  "  "BP"  "BP"  "BP"  "BP"  "BP"  "BP" ]
+         [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "  "  "  "  "BP"  "WP"  "  "  "WN"  "  "  "  " ]
+         [ "WP"  "WP"  "  "  "  "  "WP"  "WP"  "WP"  "WP" ]
+         [ "WR"  "WN"  "WB"  "WQ"  "WK"  "WB"  "  "  "WR" ]]
+        last-move ["c2" "c4"]
+        found-moves (set (map (partial map board-notation)
+                              (find-available-moves \B board last-move)))]
+    (fact "Engine knows en passant moves to the right"
+      (contains? found-moves ["b4" "c3"]) => true)
+    (fact "Engine understands results of en passant moves to the right"
+      (move board "b4" "c3") => board'))
 
 
-(deftest en-passant-test-case3
-  (testing "Engine picks en passant moves."
-    (let [board
-          [[ "  "  "  "  "  "  "  "  "BK"  "  "  "  "  "  " ]
-           [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
-           [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
-           [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
-           [ "WP"  "BP"  "  "  "  "  "  "  "  "  "  "  "  " ]
-           [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
-           [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
-           [ "  "  "  "  "  "  "  "  "WK"  "  "  "  "  "  " ]]
-          last-move ["a2" "a4"]]
-
-      (is (= (choose-best-move \B board last-move)
-             ["b4" "a3"]))
-      (is (empty-square? (get-in (move board "b4" "a3") (board-coords "a4")))))))
+  (let [board
+        [[ "  "  "  "  "  "  "  "  "BK"  "  "  "  "  "  " ]
+         [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "WP"  "BP"  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "  "  "  "  "  "  "  "  "WK"  "  "  "  "  "  " ]]
+        last-move ["a2" "a4"]]
+    (fact "Engine makes en passant moves in an obvious scenario"
+      (choose-best-move \B board last-move) => ["b4" "a3"]))
 
 
-(deftest en-passant-test-case4
-  (testing "Engine checks the last move for en passant."
-    (let [board
-          [[ "  "  "  "  "  "  "  "  "BK"  "  "  "  "  "  " ]
-           [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
-           [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
-           [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
-           [ "WP"  "BP"  "  "  "  "  "  "  "  "  "  "  "  " ]
-           [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
-           [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
-           [ "  "  "  "  "  "  "  "  "WK"  "  "  "  "  "  " ]]
-          last-move ["a3" "a4"]
-          found-pawn-moves (set (map (partial map board-notation)
-                                     (find-available-moves \B board last-move)))]
-      (is (not (contains? found-pawn-moves ["b4" "a3"])))
-      ;; obvious best move
-      (is (= (choose-best-move \B board last-move)
-             ["b4" "b3"])))))
+  (let [board
+        [[ "  "  "  "  "  "  "  "  "BK"  "  "  "  "  "  " ]
+         [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "WP"  "BP"  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "  "  "  "  "  "  "  "  "  "  "  "  "  "  "  " ]
+         [ "  "  "  "  "  "  "  "  "WK"  "  "  "  "  "  " ]]
+        last-move ["a3" "a4"]]
+    (fact "Engine checks the last move for en passant"
+      (let [found-moves (set (map (partial map board-notation)
+                                  (find-available-moves \B board last-move)))]
+        (contains? found-moves ["b4" "a3"])) => false)
+    (fact "Engine makes an obvious best move when en passant not possible"
+      (choose-best-move \B board last-move) => ["b4" "b3"])))
