75c9c2d3a43d9ec633c11296b94028bde94dde62
Lukasz Kozuchowski <Lukasz.Kozuchowski@gmail.com>
2017-02-21 04:59:49 +0100

    Cljfmt

--- src/swordfight/profiling.clj
+++ src/swordfight/profiling.clj
@@ -4,13 +4,13 @@
 (def filename "swordfight-profiling.log")
 
 (add-handler! :spitting-handler "*"
-  (fn [m]
-    (let [{:keys [stats-str_ ?id ?data]} m
-          stats-str (force stats-str_)]
-      (spit filename
-        (str
-         "\n\n"
-         (java.util.Date.)
-         (when ?id   (str "\nid: "   ?id))
-         (when ?data (str "\ndata: " ?data))
-         "\n" stats-str) :append true))))
+              (fn [m]
+                (let [{:keys [stats-str_ ?id ?data]} m
+                      stats-str (force stats-str_)]
+                  (spit filename
+                        (str
+                         "\n\n"
+                         (java.util.Date.)
+                         (when ?id   (str "\nid: "   ?id))
+                         (when ?data (str "\ndata: " ?data))
+                         "\n" stats-str) :append true))))
