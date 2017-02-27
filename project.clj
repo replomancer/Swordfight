4a19315d68404955462ffa6ae47382a79071bbdf
Lukasz Kozuchowski <Lukasz.Kozuchowski@gmail.com>
2017-02-27 10:09:43 +0100

    Urls in project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject swordfight "0.8.0-SNAPSHOT"
   :description "Chess engine"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/evalapply/Swordfight"
   :main swordfight.core
-  :license {:name "2-clause BSD"
-            :url ""}
+  :license {:name "BSD-2-clause"
+            :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]]
   :profiles {:dev {:dependencies [[midje "1.9.0-alpha6"] [com.taoensso/tufte "1.1.1"]]
                    :plugins [[lein-midje "3.2"] [lein-cljfmt "0.5.6"]]}})
